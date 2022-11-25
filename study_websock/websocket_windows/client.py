286c00e8938e0fce34692cec7e58ff5d3eb6ef07
Taebin, Yang <ksa.yangtaebin@gmail.com>
2022-11-25 13:49:50 +0900

    windows python simple socket program added

---
+++ study_websock/websocket_windows/client.py
@@ -0,0 +1,20 @@
+import socket
+
+Host = "43.201.73.94"
+port = 8000
+
+def connect_server(socket, address):
+    connection_trial = 10
+    for i in range(connection_trial):
+        try:
+            socket.connect(address)
+            print("\rconnection success")
+            break
+        except:
+            print(f"\rconnection failed... | trial: {i + 1}/{connection_trial}", end="")
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connect_server(s,(Host,port))
+s.sendall(b"Hello,world!\n")
+s.close()
