a42a1af5d1b21ccc1dd4fd25ae7b5d175848c1c2
Taebin, Yang <ksa.yangtaebin@gmail.com>
2022-11-27 00:51:30 +0900

    char_serv.cpp imporve memory leak

--- study_websock/websocket_for_unix/chat_serv.cpp
+++ study_websock/websocket_for_unix/chat_serv.cpp
@@ -41,7 +41,7 @@ void sigchld_handler(int sig) {
 int main() {
   struct sigaction sa;
   sa.sa_handler = sigchld_handler;
-  sa.sa_flags = SA_NODEFER | SA_NOCLDWAIT;
+  sa.sa_flags = 0;
   sigemptyset(&sa.sa_mask);
   sigaction(SIGCHLD, &sa, NULL);
 
