19a272448e10a5c6c6ff1d23ad081d949d1516d3
Taebin, Yang <ksa.yangtaebin@gmail.com>
2022-11-26 14:28:09 +0900

    cpp_wrapper.cpp wrote

--- study_cpp/cpp_wrapper.cpp
+++ study_cpp/cpp_wrapper.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+class A{
+public:
+  int c, d;
+  A(int a, int b) : c(a+b) d(a-b){};
+};
+int main(){
+  int a, b = 1, 2;
+  A a = A(a,b);
+  std::cout << a.c + " " + a.d << std::endl;
+}
