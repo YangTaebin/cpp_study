a72883044671835a3e97322fea16d798db04469b
Taebin, Yang <ksa.yangtaebin@gmail.com>
2022-11-26 14:45:45 +0900

    cpp_wrapper.cpp changed

--- study_cpp/cpp_wrapper.cpp
+++ study_cpp/cpp_wrapper.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 class A{
 public:
-  int c, d;
-  explicit A(int a, int b) : c(a+b), d(a-b){};
+   int num;
+   explicit A(int n) : num(n){};
 };
+void printA(A a){
+   std::cout << a.num << std::endl;
+}
 int main(){
-  int a, b;
-  a = 1;
-  b = 2;
-  A a_object(a,b);
-  std::cout << a_object.c << std::endl;
-  std::cout << a_object.d << std::endl;
+   int n = 26;
+   printA(n); //Error!
 }
